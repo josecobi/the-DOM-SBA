805a998f7ce54f33d8636e21c8d50dd9a5e2585a
josecobi <58313777+josecobi@users.noreply.github.com>
2023-12-13 13:02:46 -0500

    Added comments with pseudocode

---
+++ src/index.js
@@ -0,0 +1,76 @@
+//Create array of objects. Each object contains information of each sentence
+const sentencesArray = [
+    {
+      // Order of words to validate the answer  
+      orderOfWords: 12345,
+      hint: "The kids play...",
+      // Words that conform the sentence. Id will be used to validate the answer
+      words: [
+        { id: 1, word: "los niños" },
+        { id: 2, word: "juegan" },
+        { id: 3, word: "en" },
+        { id: 4, word: "el" },
+        { id: 5, word: "parque" },
+      ],
+    },
+    {
+      orderOfWords: 123,
+      hint: "She brushes...",
+      words: [
+        { id: 1, word: "ella" },
+        { id: 2, word: "se cepilla" },
+        { id: 3, word: "los dientes" },
+      ],
+    },
+    {
+      orderOfWords: 123,
+      hint: "We're going to...",
+      words: [
+        { id: 1, word: "el" },
+        { id: 2, word: "hombre" },
+        { id: 3, word: "corre" },
+      ],
+    },
+    {
+      orderOfWords: 123,
+      hint: "En la playa",
+      words: [
+        { id: 1, word: "el" },
+        { id: 2, word: "niño" },
+        { id: 3, word: "juega" },
+      ],
+    },
+    {
+      orderOfWords: 12345,
+      hint: "Cena familiar",
+      words: [
+        { id: 1, word: "la" },
+        { id: 2, word: "mujer" },
+        { id: 3, word: "lee" },
+        { id: 4, word: "un" },
+        { id: 5, word: "libro" },
+
+      ],
+    },
+  ];
+
+  // Create variables to manipulate the DOM
+
+  //Add event listener to the start button to start the game
+
+  //Declare the main function of the app
+  function startGame(evt){
+    // Reset app (hide feedback and next button show submit button)
+    // Call helper functions
+    // display shuffled words
+    // add event listener for drag and drop
+    // add event listener for submit button
+    // Hide submit button
+    
+    // Validate answer
+    // Provide feedback
+    // Show next button
+    // add event listener for the next button
+    //  
+  }
+  //Declare helper functions
